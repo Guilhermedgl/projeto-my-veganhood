c93080604810cd61d972e81b0267d12349c19f3c
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-11-17 16:43:26 -0200

    added mainpage.hbs, link to social login (unfinished) and user schema

---
+++ starter-code/models/user.js
@@ -0,0 +1,15 @@
+// models/User
+
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  googleID: String,
+  facebookID: String
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// GET mainpage with guest link
+router.get('/mainpage', (req, res) => {
+  res.render('../views/mainpage.hbs');
+});
+
+//router to add restaurants
+router.get('/rest-add', (req, res) => {
+  res.render('../views/rest-add.hbs');
+});
+
 module.exports = router;
