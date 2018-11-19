4943a1a8891850c084ee8ac69a36167f9326c132
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-11-19 21:57:18 -0200

    change the marker from google maps

--- starter-code/public/javascripts/main.js
+++ starter-code/public/javascripts/main.js
@@ -1,79 +1,62 @@
 window.onload = () => {
-  const ironhackBCN = {
-    lat: 41.386230,
-    lng: 2.174980,
-  };
+  function startMap() {
 
-  const markers = [];
+    // Initial coordinates
+    const initialCortinates = { lat: -23.550305,  lng: -46.6363896 };
+  
+    // Initialize the map
+    const map = new google.maps.Map(document.getElementById('map'), 
+      {
+        zoom: 15,
+        center: initialCortinates
+      }
+    );
+  
+    // Add a marker in initial cortinates
+    // const IronhackBCNMarker = new google.maps.Marker({
+    //   position: {
+    //     lat: initialCortinates.lat,
+    //     lng: initialCortinates.lng
+    //   },
+    //   map: map,
+    //   title: "Praça da Sé"
+    // });
 
-  // const map = new google.maps.Map(document.getElementById('map'), {
-  //   zoom: 13,
-  //   center: ironhackBCN,
-  // });
-
-  const directionsService = new google.maps.DirectionsService;
-const directionsDisplay = new google.maps.DirectionsRenderer;
-const mapElement = document.getElementById('map');
-
-const mapOptions = {
-  zoom: 5,
-  center: ironhackBCN
-};
-
-const map = new google.maps.Map(mapElement, mapOptions);
-
-function addMarker(title, lat, lng) {
-  return new google.maps.Marker({ position: { lat, lng }, title });
-}
-
-function getCurrentLocation() {
-  return new Promise((resolve, reject) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
-        resolve({
+        const user_location = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
+        };
+  
+        // Center map with user location
+        map.setCenter(user_location);
+  
+        // Add a marker
+        let iconMarker = {
+          url: '../images/broccoli.svg', // url
+          scaledSize: new google.maps.Size(40, 40), // size
+          origin: new google.maps.Point(0,0), // origin
+          anchor: new google.maps.Point(0, 32) // anchor
+      };
+      
+        
+        const newMarker = new google.maps.Marker({
+          position: {
+            lat: user_location.lat,
+            lng: user_location.lng
+          },
+          map: map,
+          icon: iconMarker
         });
+  
       }, function () {
-        reject('Error in the geolocation service.');
+        console.log('Error in the geolocation service.');
       });
     } else {
-      reject('Browser does not support geolocation.');
+      console.log('Browser does not support geolocation.');
     }
-  });
-}
-
-function drawRoute(src, dest) {
-  console.log('drawRoute');
-  directionsService.route({
-      origin: { lat: src.lat, lng: src.lng },
-      destination: { lat: dest.lat, lng: dest.lng },
-      travelMode: 'WALKING'
-    },
-    function(response, status) {
-      if (status === 'OK') {
-        console.log('callback called success');
-        directionsDisplay.setDirections(response);
-      } else {
-        console.log('callback called error');
-        console.log('Directions request failed due to ' + status);
-      }
-    }
-  );
-  directionsDisplay.setMap(map);
-}
-
-function start() {
-  getCurrentLocation()
-    .then(objLoc => {
-      drawRoute(objLoc, {
-        lat: -10.973229,
-        lng: -37.075218
-      })
-    })
-    .catch(err => console.log('Error on promise', err));
-  // addMarker('My first marker', 41.3977381, 2.190471916);
-}
-
-start();
+  }
+  
+  startMap();
 };
