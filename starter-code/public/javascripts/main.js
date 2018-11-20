de5b7ced575553a6550f9022bc7ca734de1c20ad
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-11-19 22:04:09 -0200

    changed the marker from google maps

--- starter-code/public/javascripts/main.js
+++ starter-code/public/javascripts/main.js
@@ -2,45 +2,34 @@ window.onload = () => {
   function startMap() {
 
     // Initial coordinates
-    const initialCortinates = { lat: -23.550305,  lng: -46.6363896 };
-  
+    const initialCortinates = {
+      lat: -23.550305,
+      lng: -46.6363896
+    };
     // Initialize the map
-    const map = new google.maps.Map(document.getElementById('map'), 
-      {
-        zoom: 15,
-        center: initialCortinates
-      }
-    );
-  
-    // Add a marker in initial cortinates
-    // const IronhackBCNMarker = new google.maps.Marker({
-    //   position: {
-    //     lat: initialCortinates.lat,
-    //     lng: initialCortinates.lng
-    //   },
-    //   map: map,
-    //   title: "Praça da Sé"
-    // });
-
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: initialCortinates
+    });
+    // Get geolocation from browser
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
         const user_location = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-  
+
         // Center map with user location
         map.setCenter(user_location);
-  
+
         // Add a marker
         let iconMarker = {
           url: '../images/broccoli.svg', // url
           scaledSize: new google.maps.Size(40, 40), // size
-          origin: new google.maps.Point(0,0), // origin
+          origin: new google.maps.Point(0, 0), // origin
           anchor: new google.maps.Point(0, 32) // anchor
-      };
-      
-        
+        };
+
         const newMarker = new google.maps.Marker({
           position: {
             lat: user_location.lat,
@@ -49,7 +38,7 @@ window.onload = () => {
           map: map,
           icon: iconMarker
         });
-  
+
       }, function () {
         console.log('Error in the geolocation service.');
       });
@@ -57,6 +46,6 @@ window.onload = () => {
       console.log('Browser does not support geolocation.');
     }
   }
-  
+
   startMap();
 };
